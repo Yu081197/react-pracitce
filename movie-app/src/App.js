ee71ae43a61b179fa952ed775cde9be9fb7890c2
Yumalup <sonaki970811@gmail.com>
2022-08-23 19:17:51 +0900

    Add : route

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
-
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
@@ -12,4 +18,5 @@ function App() {
     </Router>
   );
 }
+
 export default App;
